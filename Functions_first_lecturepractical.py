a8210cfc0f6157f459b47d33d48a85070b2817ad
rolling01 <46757419+rolling01@users.noreply.github.com>
2019-02-13 12:35:59 +0000

    Add files via upload

---
+++ Functions_first_lecturepractical.py
@@ -0,0 +1,26 @@
+#def mynewfunction (x):
+ #   print('you passed me:')
+  #  print(x)
+
+#def ReturnLargest (X,Y):
+		#if(X>Y):
+				#answer=X
+		#else:
+				#answer=Y
+		#return answer
+
+def FindSmallest(45,34,23,87,61)
+
+FindSmallest
+
+23
+
+
+
+
+  
+
+##def TimesFour(u)
+##
+##alpha = doubleIt (u)
+##print (apha*2)
